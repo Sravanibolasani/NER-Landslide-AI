ae445f46a377a92d783fcfde9ae25d997a736769
Sravani Bolasani <sravanibolasani@gmail.com>
2026-09-08 10:24:24 +0530

    Refactor image analysis script for clarity and structure

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ analyzeBtn.addEventListener("click", async function () {
     try {
 
         const response = await fetch(
-           "https://ner-landslide-backend-cr6k.onrender.com/predict"
+            "https://ner-landslide-backend-cr6k.onrender.com/predict",
             {
                 method: "POST",
                 body: formData
@@ -81,12 +81,12 @@ analyzeBtn.addEventListener("click", async function () {
 
         }
 
-   } catch (error) {
+    } catch (error) {
 
-    alert("AI analysis failed: " + error.message);
+        alert("AI analysis failed: " + error.message);
+        console.error(error);
 
-    console.error(error);
-}
+    }
 
     analyzeBtn.innerText = "ANALYZE WITH AI";
     analyzeBtn.disabled = false;
